f55c422221c98a671aebafb83a7e214803f10e94
Константин Игнатьев <ignatyev.k@yandex.ru>
2022-08-03 23:27:27 +0300

    task done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿void CreateNewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string[5] { "мир", "труд", "май", "весна", "цветы" };
+string[] array2 = new string[array1.Length];
+
+CreateNewArray(array1, array2);
+
+ShowArray(array2);
